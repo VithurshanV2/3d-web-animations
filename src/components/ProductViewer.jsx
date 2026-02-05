94997c6ed39897371e500acf477c52190e5c5cdb
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2026-02-05 10:01:50 +0530

    Add 3D product viewer with macbook model
    - render macbook model using react three fiber
    - add interactive 3d scene to product viewer

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,5 +1,8 @@
 import clsx from 'clsx';
 import useMacbookStore from '../store';
+import MacbookModel14 from './models/Macbook-14';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -57,6 +60,17 @@ const ProductViewer = () => {
           </div>
         </div>
       </div>
+
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <ambientLight intensity={0.5} />
+
+        <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
+
+        <OrbitControls enableZoom={false} />
+      </Canvas>
     </section>
   );
 };
